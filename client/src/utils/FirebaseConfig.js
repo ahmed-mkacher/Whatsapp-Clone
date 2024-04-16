793f85ddbf82489724cb94f7a0a9e55affea5c85
ahmed-mkacher <mkacher.ahmed@outlook.com>
2024-04-16 18:53:02 +0100

    fix

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -1,6 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-VYJOj86IngFaSTlzqnuOVWfPFwUbK_4",
@@ -9,7 +8,7 @@ const firebaseConfig = {
   storageBucket: "whatsapp-clone-768ae.appspot.com",
   messagingSenderId: "1093255963882",
   appId: "1:1093255963882:web:1434a395342a7bdc69e393",
-  measurementId: "G-HV31W846Q3"
+  measurementId: "G-HV31W846Q3",
 };
 
 const app = initializeApp(firebaseConfig);
