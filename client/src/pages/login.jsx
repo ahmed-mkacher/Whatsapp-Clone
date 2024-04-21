6b1188e873a804f4c23b9f1ffeb674cf929dd11d
ahmed-mkacher <mkacher.ahmed@outlook.com>
2024-04-21 02:11:50 +0100

    finished login, and almost onboarding page, no bugs

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import Image from "next/image";
+import axios from "axios";
+import { FcGoogle } from "react-icons/fc";
+import { GoogleAuthProvider, signInWithPopup, auth } from "firebase/auth";
+import { firebaseAuth } from "@/utils/FirebaseConfig";
+import { CHECK_USER_ROUTE } from "@/utils/ApiRoutes";
+import { useRouter } from "next/router";
+import { useStateProvider } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
 
 function login() {
-  return <div>login</div>;
+  const router = useRouter();
+
+  const [{}, dispatch] = useStateProvider();
+
+  const handleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(firebaseAuth, provider);
+    const {
+      user: { displayName: name, email, photoUrl: profileImage },
+    } = result;
+    try {
+      if (email) {
+        const { data } = await axios.post(CHECK_USER_ROUTE, { email });
+
+        if (!data.status) {
+          dispatch({ type: reducerCases.SET_NEW_USER, newUser: true });
+          dispatch({
+            type: reducerCases.SET_USER_INFO,
+            userInfo: {
+              name,
+              email,
+              profileImage,
+              status: "",
+            },
+          });
+          router.push("/onboarding");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(result.user);
+  };
+
+  return (
+    <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
+      <div className="flex items-center justify-center gap-2 text-white">
+        <Image
+          src="/whatsapp.gif"
+          alt="Whatsapp logo"
+          height={300}
+          width={300}
+        />
+        <span className="text-7xl">Whatsapp</span>
+      </div>
+
+      <button
+        className="flex justify-center items-center gap-7 bg-search-input-container-background p-5 rounded-lg"
+        onClick={handleLogin}
+      >
+        <FcGoogle className="text-4xl" />
+        <span className="text-white text-2xl">Login with Google</span>
+      </button>
+    </div>
+  );
 }
 
 export default login;
